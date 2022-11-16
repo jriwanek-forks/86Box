e771aa007d9cee78d2b8458b27074cbb52bd5e9c
Jasmine Iwanek <jasmine@iwanek.co.uk>
2022-11-16 02:04:28 -0500

    Atari PC 3

--- src/machine/m_xt.c
+++ src/machine/m_xt.c
@@ -213,6 +213,26 @@ machine_xt_amixt_init(const machine_t *model)
 }
 
 int
+machine_xt_ataripc3_init(const machine_t *model)
+{
+    int ret;
+
+#if 0
+    ret = bios_load_linear("roms/machines/ataripc3/AWARD_ATARI_PC_BIOS_3.08.BIN",
+                           0x000f8000, 32768, 0);
+#endif
+    ret = bios_load_linear("roms/machines/ataripc3/c101701-004 308.u61",
+                           0x000f8000, 0x8000, 0);
+
+    if (bios_only || !ret)
+        return ret;
+
+    machine_xt_clone_init(model, 0);
+
+    return ret;
+}
+
+int
 machine_xt_znic_init(const machine_t *model)
 {
     int ret;
