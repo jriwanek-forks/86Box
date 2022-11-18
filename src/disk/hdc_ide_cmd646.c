14941dcf46279314291c47fd2cfa270588c02e30
Jasmine Iwanek <jasmine@iwanek.co.uk>
2022-11-18 14:28:34 -0500

    Initial Imation SuperDisk support

--- src/disk/hdc_ide_cmd646.c
+++ src/disk/hdc_ide_cmd646.c
@@ -35,6 +35,7 @@
 #include <86box/hdc_ide.h>
 #include <86box/hdc_ide_sff8038i.h>
 #include <86box/zip.h>
+#include <86box/superdisk.h>
 #include <86box/mo.h>
 
 typedef struct cmd646_t {
@@ -320,10 +321,17 @@ cmd646_reset(void *priv)
         if ((cdrom[i].bus_type == CDROM_BUS_ATAPI) && (cdrom[i].ide_channel < 4) && cdrom[i].priv)
             scsi_cdrom_reset((scsi_common_t *) cdrom[i].priv);
     }
+
     for (i = 0; i < ZIP_NUM; i++) {
         if ((zip_drives[i].bus_type == ZIP_BUS_ATAPI) && (zip_drives[i].ide_channel < 4) && zip_drives[i].priv)
             zip_reset((scsi_common_t *) zip_drives[i].priv);
     }
+
+    for (i = 0; i < SUPERDISK_NUM; i++) {
+        if ((superdisk_drives[i].bus_type == SUPERDISK_BUS_ATAPI) && (superdisk_drives[i].ide_channel < 4) && superdisk_drives[i].priv)
+            superdisk_reset((scsi_common_t *) superdisk_drives[i].priv);
+    }
+
     for (i = 0; i < MO_NUM; i++) {
         if ((mo_drives[i].bus_type == MO_BUS_ATAPI) && (mo_drives[i].ide_channel < 4) && mo_drives[i].priv)
             mo_reset((scsi_common_t *) mo_drives[i].priv);
