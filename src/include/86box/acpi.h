7100452663b3233f6e981dec170dce7b5928bf47
Jasmine Iwanek <jasmine@iwanek.co.uk>
2023-09-01 16:23:20 -0400

    SIS 5598

--- src/include/86box/acpi.h
+++ src/include/86box/acpi.h
@@ -142,6 +142,7 @@ extern int        acpi_enabled;
 
 extern const device_t acpi_ali_device;
 extern const device_t acpi_intel_device;
+extern const device_t acpi_sis_device;
 extern const device_t acpi_smc_device;
 extern const device_t acpi_via_device;
 extern const device_t acpi_via_596b_device;
