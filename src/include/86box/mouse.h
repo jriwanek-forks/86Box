2f4073f3b09b29d9dbb738a285f2c5fd50f52337
Cacodemon345 <wahil1976@outlook.com>
2023-11-16 15:43:39 +0600

    Ready for PR

--- src/include/86box/mouse.h
+++ src/include/86box/mouse.h
@@ -74,7 +74,9 @@ extern const device_t mouse_ps2_device;
 extern const device_t mouse_wacom_device;
 extern const device_t mouse_wacom_artpad_device;
 extern const device_t mouse_mtouch_device;
+#ifdef USE_USB_MOUSE
 extern const device_t mouse_usb_device;
+#endif
 #endif
 
 extern void            mouse_clear_x(void);
