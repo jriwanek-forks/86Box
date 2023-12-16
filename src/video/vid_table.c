6b275cdb588351c42fd189038da8aeef6d30b9a7
Jasmine Iwanek <jasmine@iwanek.co.uk>
2023-12-16 13:44:23 -0500

    Re-add removed flag

--- src/video/vid_table.c
+++ src/video/vid_table.c
@@ -209,7 +209,7 @@ video_cards[] = {
     { &s3_virge_357_pci_device                         },
     { &s3_diamond_stealth_4000_pci_device              },
     { &s3_trio3d2x_pci_device                          },
-    { &millennium_device                               },
+    { &millennium_device,      VIDEO_FLAG_TYPE_SPECIAL },
     { &millennium_ii_device                            },
     { &mystique_device                                 },
     { &mystique_220_device                             },
