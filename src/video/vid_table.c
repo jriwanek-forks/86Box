866f947e3dd0544bd2d814093b4afc31e7a7454f
Jasmine Iwanek <jasmine@iwanek.co.uk>
2023-12-29 22:58:13 -0500

    Add Genoa 3D (Virge) video card

--- src/video/vid_table.c
+++ src/video/vid_table.c
@@ -204,6 +204,7 @@ video_cards[] = {
     { &s3_stb_velocity_3d_pci_device                   },
     { &s3_virge_375_pci_device                         },
     { &s3_diamond_stealth_2000pro_pci_device           },
+    { &s3_genoa_3d_pci_device                          },
     { &s3_virge_385_pci_device                         },
     { &s3_virge_357_pci_device                         },
     { &s3_diamond_stealth_4000_pci_device              },
