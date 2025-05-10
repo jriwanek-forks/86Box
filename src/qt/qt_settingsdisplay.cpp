a8d424fbce1027941c7064648b258c02b6f069e2
Jasmine Iwanek <jasmine@iwanek.co.uk>
2025-05-09 20:45:24 -0400

    Clean QT Display UI

--- src/qt/qt_settingsdisplay.cpp
+++ src/qt/qt_settingsdisplay.cpp
@@ -51,22 +51,29 @@ SettingsDisplay::~SettingsDisplay()
 void
 SettingsDisplay::save()
 {
-    gfxcard[0]                 = ui->comboBoxVideo->currentData().toInt();
     // TODO
+#if 0
+    for (uint8_t i = 0; i < GFXCARD_MAX; ++i) {
+        QComboBox *cbox = findChild<QComboBox *>(QString("comboBoxVideo%1").arg(i + 1));
+        gfxcard[i]      = cbox->currentData().toInt();
+    }
+#else
+    gfxcard[0] = ui->comboBoxVideo->currentData().toInt();
     for (uint8_t i = 1; i < GFXCARD_MAX; i ++)
-        gfxcard[i]                 = ui->comboBoxVideoSecondary->currentData().toInt();
+        gfxcard[i] = ui->comboBoxVideoSecondary->currentData().toInt();
+#endif
 
     voodoo_enabled             = ui->checkBoxVoodoo->isChecked() ? 1 : 0;
     ibm8514_standalone_enabled = ui->checkBox8514->isChecked() ? 1 : 0;
     xga_standalone_enabled     = ui->checkBoxXga->isChecked() ? 1 : 0;
-    da2_standalone_enabled = ui->checkBoxDa2->isChecked() ? 1 : 0;
+    da2_standalone_enabled     = ui->checkBoxDa2->isChecked() ? 1 : 0;
 }
 
 void
 SettingsDisplay::onCurrentMachineChanged(int machineId)
 {
     // win_settings_video_proc, WM_INITDIALOG
-    this->machineId = machineId;
+    this->machineId   = machineId;
     auto curVideoCard = videoCard[0];
 
     auto *model      = ui->comboBoxVideo->model();
@@ -98,6 +105,7 @@ SettingsDisplay::onCurrentMachineChanged(int machineId)
     }
     model->removeRows(0, removeRows);
 
+    // TODO
     if (machine_has_flags(machineId, MACHINE_VIDEO_ONLY) > 0) {
         ui->comboBoxVideo->setEnabled(false);
         ui->comboBoxVideoSecondary->setEnabled(false);
@@ -160,9 +168,9 @@ SettingsDisplay::on_pushButtonConfigureDa2_clicked()
 void
 SettingsDisplay::on_comboBoxVideo_currentIndexChanged(int index)
 {
-    if (index < 0) {
+    if (index < 0)
         return;
-    }
+
     static QRegularExpression voodooRegex("3dfx|voodoo|banshee", QRegularExpression::CaseInsensitiveOption);
     auto curVideoCard_2 = videoCard[1];
     videoCard[0] = ui->comboBoxVideo->currentData().toInt();
