d7c95634a3db597345e477c898ded74c13760bbe
Jasmine Iwanek <jasmine@iwanek.co.uk>
2025-05-14 17:16:17 -0400

    Initial IBM SDLC Support

---
+++ src/include/86box/sdlc.h
@@ -0,0 +1,21 @@
+/*
+ * 86Box    A hypervisor and IBM PC system emulator that specializes in
+ *          running old operating systems and software designed for IBM
+ *          PC systems and compatibles from 1981 through fairly recent
+ *          system designs based on the PCI bus.
+ *
+ *          This file is part of the 86Box distribution.
+ *
+ *          Definitons for IBM SDLC emulation.
+ *
+ * Authors: Jasmine Iwanek, <jasmine@iwanek.co.uk>
+ *
+ *          Copyright 2022-2025 Jasmine Iwanek.
+ */
+
+#ifndef EMU_SDLC_H
+#define EMU_SDLC_H
+
+extern const device_t ibm_sdlc_device;
+
+#endif /*EMU_SDLC_H*/
