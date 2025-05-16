4164264b09d6f5e08f5c98b73151f2a859d8c41c
Jasmine Iwanek <jasmine@iwanek.co.uk>
2025-05-16 19:16:56 -0400

    Cleanups & Comments in video.c/h

--- src/video/video.c
+++ src/video/video.c
@@ -1017,8 +1017,8 @@ loadfont_common(FILE *fp, int format)
                 for (uint8_t d = 0; d < 8; d++)
                     fontdatm[c][d + 8] = fgetc(fp) & 0xff;
             (void) fseek(fp, 4096 + 2048, SEEK_SET);
-            for (uint16_t c = 0; c < 256; c++)
-                for (uint8_t d = 0; d < 8; d++) /* 8x8 CGA (thick, primary) */
+            for (uint16_t c = 0; c < 256; c++) /* 8x8 CGA (thick, primary) */
+                for (uint8_t d = 0; d < 8; d++)
                     fontdat[c][d] = fgetc(fp) & 0xff;
             break;
 
@@ -1036,7 +1036,7 @@ loadfont_common(FILE *fp, int format)
 
         default:
         case 2: /* CGA */
-            for (uint16_t c = 0; c < 256; c++)
+            for (uint16_t c = 0; c < 2048; c++) /* Allow up to 2048 chars */ // TODO: Was 256
                 for (uint8_t d = 0; d < 8; d++)
                     fontdat[c][d] = fgetc(fp) & 0xff;
             break;
