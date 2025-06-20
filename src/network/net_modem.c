276eda70968b025ad2905c6492bfff81b73702e7
Jasmine Iwanek <jasmine@iwanek.co.uk>
2025-06-19 21:19:40 -0400

    Correct comment

--- src/network/net_modem.c
+++ src/network/net_modem.c
@@ -1110,7 +1110,7 @@ modem_do_command(modem_t *modem, int repeat)
                     break;
                 }
             case '%': // % escaped commands
-                // Windows 98 modem prober sends unknown command AT%V
+                // Windows 95 and 98 modem prober sends unknown command AT%V
                 modem_send_res(modem, ResERROR);
                 return;
             case '\0':
