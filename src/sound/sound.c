b2394b0f57741fce692d26e55c7fc57ce2b97bce
Jasmine Iwanek <jasmine@iwanek.co.uk>
2025-09-06 21:35:44 -0400

    IMFC is now available, but still broken
    Co-Authored-By: Cacodemon345 <38420290+Cacodemon345@users.noreply.github.com>

--- src/sound/sound.c
+++ src/sound/sound.c
@@ -121,6 +121,7 @@ static const SOUND_CARD sound_cards[] = {
     { &ess_1688_device              },
     { &ess_ess0102_pnp_device       },
     { &ess_ess0968_pnp_device       },
+    { &imfc_card_device             },
     { &ssi2001_device               },
     { &mmb_device                   },
 #ifdef USE_LIBSERIALPORT /*The following devices required LIBSERIALPORT*/
