5f296e978e229a222172e07d8634a6f97b057302
Jasmine Iwanek <jasmine@iwanek.co.uk>
2025-10-21 16:28:00 -0400

    Revert "Swap axis names on CH products things with pedals"
    This reverts commit 6339f65087412b84ae69156036233cfcc6c486cb.

--- src/game/joystick_ch_flightstick_pro.c
+++ src/game/joystick_ch_flightstick_pro.c
@@ -191,7 +191,7 @@ const joystick_t joystick_ch_flightstick_ch_pedals_pro = {
     .button_count  = 2,
     .pov_count     = 0,
     .max_joysticks = 1,
-    .axis_names    = { "X axis (Roll)", "Y axis (Pitch)", "Left Pedal", "Right Pedal" },
+    .axis_names    = { "X axis (Roll)", "Y axis (Pitch)", "Right Pedal", "Left Pedal" },
     .button_names  = { "Trigger", "Button 2" },
     .pov_names     = { NULL }
 };
@@ -245,7 +245,7 @@ const joystick_t joystick_ch_flightstick_pro_ch_pedals_pro = {
     .button_count  = 4,
     .pov_count     = 1,
     .max_joysticks = 1,
-    .axis_names    = { "X axis (Roll)", "Y axis (Pitch)", "Left Pedal", "Right Pedal" },
+    .axis_names    = { "X axis (Roll)", "Y axis (Pitch)", "Right Pedal", "Left Pedal" },
     .button_names  = { "Trigger", "Button 2", "Button 3", "Button 4" },
     .pov_names     = { "POV" }
 };
@@ -299,7 +299,7 @@ const joystick_t joystick_ch_virtual_pilot_ch_pedals_pro = {
     .button_count  = 2,
     .pov_count     = 0,
     .max_joysticks = 1,
-    .axis_names    = { "X axis (Roll)", "Y axis (Pitch)", "Left Pedal", "Right Pedal" },
+    .axis_names    = { "X axis (Roll)", "Y axis (Pitch)", "Right Pedal", "Left Pedal" },
     .button_names  = { "Button 1", "Button 2" },
     .pov_names     = { NULL }
 };
@@ -353,7 +353,7 @@ const joystick_t joystick_ch_virtual_pilot_pro_ch_pedals_pro = {
     .button_count  = 6,
     .pov_count     = 2,
     .max_joysticks = 1,
-    .axis_names    = { "X axis (Roll)", "Y axis (Pitch)", "Left Pedal", "Right Pedal" },
+    .axis_names    = { "X axis (Roll)", "Y axis (Pitch)", "Right Pedal", "Left Pedal" },
     .button_names  = { "Button 1", "Button 2", "Button 3", "Button 4", "Button 5", "Button 6" },
     .pov_names     = { "Right POV", "Left POV" }
 };
